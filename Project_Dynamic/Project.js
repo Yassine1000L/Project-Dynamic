20d67dbe8276e8a105dde89c191a2032197d1f3f
Yassine1000L <yassneyas11@gmail.com>
2026-04-02 14:51:15 +0200

    async funtie haaldataop en fucntie toonData aangemaakt

---
+++ Project_Dynamic/Project.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const URL = "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/parcours_street_art/records?limit=25";
+
+let alleData = [];
+
+async function haalDataOp() {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+
+        alleData = data.results;
+        toonData(alleData);
+
+    } catch (fout) {
+        console.error(fout);
+    }
+}
+
+
+function toonData(items) {
+    const container = document.getElementById("resultaten");
+    container.innerHTML = "";
+
+    for (let item of items) {
+        const kaart = document.createElement("div");
+        kaart.classList.add("card");
+
+        kaart.innerHTML = `
+            <h3>${item.name_en || "Geen naam"}</h3>
+            <p>${item.artist_name || "Onbekend"}</p>
+        `;
+
+
+        container.appendChild(kaart);
+    }
+}
+
+    haalDataOp();
