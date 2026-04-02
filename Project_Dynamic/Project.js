ec9882b2d0e9cdd012134a1aa4ad3228a9390e6d
Hisham <boussofhisham@gmail.com>
2026-04-02 15:12:14 +0200

    update

--- Project_Dynamic/Project.js
+++ Project_Dynamic/Project.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 const URL = "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/parcours_street_art/records?limit=25";
-=======
->>>>>>> d0b3c82332e57da1db8d833bb633c5adf8f5b7b2
