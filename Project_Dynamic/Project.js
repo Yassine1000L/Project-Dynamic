3bde687078e86fa4da6d9927ebf945351ade9344
Hisham <boussofhisham@gmail.com>
2026-04-02 15:22:17 +0200

    aantal data streetarts gereduceerd tot 24

--- Project_Dynamic/Project.js
+++ Project_Dynamic/Project.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const URL = "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/parcours_street_art/records?limit=25";
+const URL = "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/parcours_street_art/records?limit=24";
 
 
 let alleData = [];
