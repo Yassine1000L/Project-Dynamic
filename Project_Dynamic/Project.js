81f9af497bf4d7195c5b39b0b7bacc4574f90e14
Hisham <boussofhisham@gmail.com>
2026-04-02 17:59:14 +0200

    twee sorteerfuncties toegevoegd om de streetartsnamen en artiesten alfabetisch en omgekeerd alfabetisch te rangschikken

--- Project_Dynamic/Project.js
+++ Project_Dynamic/Project.js
@@ -59,4 +59,67 @@ function zoekNamenOp() {
 const zoekbalk = document.getElementById("zoekbalk");
 if (zoekbalk) zoekbalk.addEventListener("input", zoekNamenOp);
 
+
+
+
+function sorteerOpStreetartsnaam() {
+    const keuzeNaam = document.getElementById("sorteerOpNaam").value;
+
+    if (keuzeNaam === "keuze") return; 
+    
+    document.getElementById("sorteerOpArtiest").value = "keuze";
+
+    alleData.sort((a, b) => {
+        let naam1 = (a.name_nl || "").toLowerCase();
+        let naam2 = (b.name_nl || "").toLowerCase();
+
+        if (keuzeNaam === "dalend") {
+            if (naam1 < naam2) return -1;
+            if (naam1 > naam2) return 1;
+            return 0;
+        } else {
+            if (naam1 > naam2) return -1;
+            if (naam1 < naam2) return 1;
+            return 0;
+        }
+    });
+
+    toonData(alleData);
+}
+
+function sorteerOpArtiest() {
+
+    const keuzeArtiest = document.getElementById("sorteerOpArtiest").value;
+    
+    if (keuzeArtiest === "keuze") return; 
+    
+    document.getElementById("sorteerOpNaam").value = "keuze";
+
+    alleData.sort((a, b) => {
+        let artiest1 = (a.artist_name || "").toLowerCase();
+        let artiest2 = (b.artist_name || "").toLowerCase();
+
+        if (keuzeArtiest === "dalend") {
+            if (artiest1 < artiest2) return -1;
+            if (artiest1 > artiest2) return 1;
+            return 0;
+        } else {
+            if (artiest1 > artiest2) return -1;
+            if (artiest1 < artiest2) return 1;
+            return 0;
+        }
+    });
+
+    toonData(alleData);
+}
+
+const sorteerknopStreetartsnaam = document.getElementById("sorteerOpNaam");
+const sorteerknopArtiest = document.getElementById("sorteerOpArtiest");
+
+if (sorteerknopStreetartsnaam) sorteerknopStreetartsnaam.addEventListener("change", sorteerOpStreetartsnaam);
+if (sorteerknopArtiest) sorteerknopArtiest.addEventListener("change", sorteerOpArtiest);
+
+
+
+
 haalDataOp();
